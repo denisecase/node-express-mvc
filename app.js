81c92ec0be0305c75f9f43b6af3ceb8d59a4eb6c
Denise Case <dcase@nwmissouri.edu>
2019-07-30 00:08:06 -0500

    lodash fix

--- app.js
+++ app.js
@@ -6,28 +6,30 @@
  * 
  * The server-side app starts and begins listening for events.
  **/
-const http = require('http')  // use require to import the Node.js http package
-const express = require('express')  // require Express framework
+const http = require('http')  
+const express = require('express')  
 const app = express()  // create an Express web app
 const server = http.createServer(app)  // pass in the Express app to our http server
 const path = require('path')
+const config = require('config')
 
-const hostname = '0.0.0.0'    // allows access from remote computers
-const port = 3004;
-
-// initialize data ............................................
-require('./utils/seeder.js')(app)  // load seed data
+// Use hosting values if available, otherwise default 
+const environment = process.env.NODE_ENV || 'development'
+const hostname = process.env.HOSTNAME || config.get("hostname")
+const port = process.env.PORT || config.get("port")
 
 // By default, Express does not serve static files. 
-// Configure middleware with app.use
-// use '/public to access files in the 'public' folder
-app.use('/public', express.static(path.join(__dirname, 'public')));
+// use middleware to define a static assets folder 'public'
+app.use(express.static('public'))
 
+// load seed data
+require('./utils/seeder.js')(app)  
+
+// Use Express middleware to configure routing
 const routing = require('./routes/router.js')
-app.use('/', routing)  // load routing
+app.use('/', routing)  
 
-server.listen(port, hostname, () => {
-  // Tell the user where to find the app (use backtics with variables)
+app.listen(port, hostname, () => {
   console.log(`App running at http://${hostname}:${port}/`)
-  console.log('Press CTRL-C to stop\n')
+  console.log('Hit CTRL-C CTRL-C to stop\n')
 })
--- routes/router.js
+++ routes/router.js
@@ -16,18 +16,24 @@ const router = express.Router()
 // Manage top-level request first
 router.get('/', (req, res, next) => {
   console.log('Request to /')
-  res.sendFile(path.join(__dirname, '../public/newPage.html'))
+  res.sendFile('index.html')
 })
 
 router.get('/index', (req, res, next) => {
-  console.log('Request to /')
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  console.log('Request to /index')
+  res.sendFile('index.html')
 })
 
 router.get('/newpage', (req, res, next) => {
   console.log('Request to /newpage')
-  res.sendFile(path.join(__dirname, '../public/sample.html'))
+  res.sendFile('newPage.html')
 })
+
+router.get('/sample', (req, res, next) => {
+  console.log('Request to /sample')
+  res.sendFile('sample.html')
+})
+
 
 
 // Defer path requests to a particular controller
